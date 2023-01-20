1a12a7b3e41a34be925cdccfb759e8c871174205
Felix Kling <felix@felix-kling.de>
2023-01-20 21:19:59 +0100

    Fix Wildcard button variant default value

--- client/web-sveltekit/src/lib/wildcard/Button.svelte
+++ client/web-sveltekit/src/lib/wildcard/Button.svelte
@@ -2,7 +2,7 @@
 	import type { BUTTON_DISPLAY, BUTTON_SIZES, BUTTON_VARIANTS } from "@sourcegraph/wildcard/src/components/Button";
     import {getButtonClassName} from '@sourcegraph/wildcard/src/components/Button/utils'
 
-    export let variant: typeof BUTTON_VARIANTS[number]|undefined = undefined
+    export let variant: typeof BUTTON_VARIANTS[number] = 'primary'
     export let size: typeof BUTTON_SIZES[number]|undefined = undefined
     export let display: typeof BUTTON_DISPLAY[number]|undefined = undefined
     export let outline: boolean|undefined = undefined
