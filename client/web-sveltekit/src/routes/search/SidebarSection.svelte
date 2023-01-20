d223ff19d7c49b3a905d0972581e29f9d149f74e
Felix Kling <felix@felix-kling.de>
2023-01-20 21:07:34 +0100

    Improve search sidebar styling

--- client/web-sveltekit/src/routes/search/SidebarSection.svelte
+++ client/web-sveltekit/src/routes/search/SidebarSection.svelte
@@ -22,37 +22,47 @@
 </script>
 
 {#if sections}
-    <Button variant="secondary" outline>
-        <button slot="custom" let:className class="{className} header" type="button" on:click={toggleSection}>
-            <header><h5>{title}</h5></header>
-            <Icon svgPath={open ? mdiChevronUp : mdiChevronDown} inline --color="var(--icon-color)" />
-        </button>
-    </Button>
+    <article>
+        <Button variant="secondary" outline>
+            <button
+                slot="custom"
+                let:className
+                class="{className} header"
+                type="button"
+                aria-expanded={open}
+                aria-label="{open ? 'Collapse' : 'Expand'} {title}"
+                on:click={toggleSection}>
+                <header><h5>{title}</h5></header>
+                <Icon svgPath={open ? mdiChevronUp : mdiChevronDown} inline --color="var(--icon-color)" />
+            </button>
+        </Button>
 
-    {#if open}
-        <ul>
-            {#each items as item}
-                <li>
-                    <Button variant="secondary" outline>
-                        <button slot="custom" let:className class="{className} item" on:click data-value={item.value} data-run={item.runImmediately}>
-                            <span class="label">{item.label}</span>
-                            {#if item.count !== undefined}
-                                <Tooltip tooltip="At least {item.count} results match this filter.">
-                                    <span class="count">{item.count}</span>
-                                </Tooltip>
-                            {/if}
-                        </button>
-                    </Button>
-                </li>
-            {/each}
-        </ul>
-    {/if}
+        {#if open}
+            <ul>
+                {#each items as item}
+                    <li>
+                        <Button variant="secondary" outline>
+                            <button slot="custom" let:className class="{className} item" on:click data-value={item.value} data-run={item.runImmediately}>
+                                <span class="label">{item.label}</span>
+                                {#if item.count !== undefined}
+                                    <Tooltip tooltip="At least {item.count} results match this filter.">
+                                        <span class="count">{item.count}</span>
+                                    </Tooltip>
+                                {/if}
+                            </button>
+                        </Button>
+                    </li>
+                {/each}
+            </ul>
+        {/if}
+    </article>
 {/if}
 
 <style lang="scss">
     ul {
         margin: 0;
-        padding: 0;
+        padding: 0.125rem;;
+        padding-bottom: 1rem;
         list-style: none;
     }
 
@@ -66,7 +76,7 @@
 
     button.header {
         padding: 0.25rem;
-        margin: 0 -0.25rem;
+        margin: 0.125rem 0;
 
         header {
             flex: 1;
